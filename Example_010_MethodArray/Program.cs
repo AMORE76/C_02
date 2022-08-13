b639c7dba490708541b2af34984cfdd201136f89
AMORE76 <gemenius007@internet.ru>
2022-08-13 12:01:09 +0300

    Added new folder Example_010_MethodArray

---
+++ Example_010_MethodArray/Program.cs
@@ -0,0 +1,26 @@
+﻿/*Есть массив array, в котором n элементов. Найти элемент, совпадающий с
+некоторым значением, который определяет пользователь.*/
+
+/*               0   1   2   3   4   5   6   7   8 */
+int[] array = { 11, 21, 31, 41, 15, 61, 11, 18, 19 };
+
+int n = array.Length;
+int find = 11;
+int index = 0;
+
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Number is absent");
+        break;
+    }
+
+    index = index + 1;
+}
