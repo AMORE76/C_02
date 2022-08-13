48cd03ca26c36c36f1940f278e4a9e9364e626b2
AMORE76 <gemenius007@internet.ru>
2022-08-13 12:34:51 +0300

    Added new folder Example_011_ArrayLibrary

---
+++ Example_011_ArrayLibrary/Program.cs
@@ -0,0 +1,45 @@
+﻿/* Перепишем код с
+использованием генератора псевдослучайных чисел с использованием методов. 
+Взять метод, передать в него массив и
+заполнить массив нужным количеством элементов. 
+Описать метод,
+который будет выводить все элементы по порядку. 
+Превратить наш код поиска нужного
+индекса в метод. */
+
+int[] array = new int[10];
+
+/*«Cоздай новый массив, где будет 10 элементов». По
+умолчанию, он будет наполнен нулями*/
+
+void FillArray(int[] collection)
+
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        //index = index + 1;
+        index++;
+    }
+}
+
+// Оформляем метод void, который будет печатать массив.
+
+void PrintArray(int[] col)
+
+{int count = col.Length;
+int position = 0;
+
+while (position < count)
+
+{Console.WriteLine(col[position]);
+position++;}
+}
+
+FillArray(array);
+PrintArray(array);
+
+/* Определен массив из 10 элементов. Вызван метод FillArray,
+который заполнил массив, вызван метод PrintArray, который распечатал массив.*/
